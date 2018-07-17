e44f03f96019fcba7363424577f787232e7290d1
josewails <joseritch45@gmail.com>
2018-07-17 09:47:54 +0300

    databases default

--- hackway/settings/production.py
+++ hackway/settings/production.py
@@ -4,7 +4,9 @@ from .base import *
 SECRET_KEY = os.environ.get('SECRET_KEY')
 PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 
-DATABASES = {}
+DATABASES = {
+    'default': {}
+}
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
