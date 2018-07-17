d8a570769c42b234d3a7f03996fc2bb37268fb7e
josewails <joseritch45@gmail.com>
2018-07-17 11:13:58 +0300

    s3 configuration

--- hackway/settings/production.py
+++ hackway/settings/production.py
@@ -15,7 +15,7 @@ DATABASES['default'].update(db_from_env)
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = 'yumm-pail'
+AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET-NAME']
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
